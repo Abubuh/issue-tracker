527669d0eb82fbac2425885a6559f1dc43fcdf03
Cristian <cristian_caht@hotmail.com>
2022-12-12 12:29:24 -0600

    Render new card component

--- app/src/components/Project/Column.tsx
+++ app/src/components/Project/Column.tsx
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 
-export const Column = ({title, addCard, ...props}) => {
+export const Column = ({title, columnNumber, dispatch, ...props}) => {
+
+  const addCard = () => {
+    console.log("Helo")
+    const card = {
+        id: self.crypto.randomUUID() ,
+        title: "New card",
+        description: "None",
+        columnNumber: columnNumber
+    }
+    dispatch({type:"addCard", card})
+}
 
   return (
     <div>
@@ -14,3 +25,7 @@ export const Column = ({title, addCard, ...props}) => {
     </div>
   );
 };
+function dispatch(arg0: { type: string; card: { id: string; title: string; description: string; }; }) {
+  throw new Error("Function not implemented.");
+}
+
--- app/src/components/ProjectView.tsx
+++ app/src/components/ProjectView.tsx
@@ -3,74 +3,82 @@ import { Board } from "./Project/Board";
 import { Header } from "./Project/Header";
 import { Column } from "./Project/Column";
 import { Card } from "./Project/Card";
-import { AddColumn } from './Project/AddColumn'
+import { AddColumn } from "./Project/AddColumn";
+
+const reducer = (columns, action) => {
+  switch (action.type) {
+    case "addCard":
+      return columns.map((column, index) => {
+        if (index + 1 === action.card.columnNumber) {
+          return {...column, cards: [...column.cards, action.card]}
+        }
+        return column;
+      });
+
+    default:
+      return columns;
+  }
+};
 
 export const ProjectView = () => {
-    const [cards, setCards] =useState([])
-    const [columns, setColumns] =useState([
+  const [cards, setCards] = useState([]);
+  const [columns, dispatch] = useReducer(reducer, [
+    {
+      title: "component1",
+      cards: [
         {
-            title : "component1",
-            cards: [
-                {
-                    id: 1,
-                    title : "Create stuff",
-                    description : "Do this"
-                }
-            ]
+          id: 1,
+          title: "Create stuff",
+          description: "Do this",
         },
+      ],
+    },
+    {
+      title: "component 2",
+      cards: [
         {
-            title : "component 2",
-            cards: [
-                {
-                    id: 2,
-                    title : "Create stuff 2",
-                    description: "Do that"
-                }
-            ]
-        }
-    ])
+          id: 2,
+          title: "Create stuff 2",
+          description: "Do that",
+        },
+      ],
+    },
+  ]);
 
-    const reducer = (columns, action) => {
-        switch (action.type){
-            case "CARDS":
-                return columns.map((card) => {
-                    if (card.id === action.id ){
-                        return {...card, title: !card.title};
-                    }else{
-                        return card;
-                    }
-                });
-                default:
-                    return columns
-            }
-        };
-
-        function Cards() {
-            const [cards, dispatch] = useReducer(reducer, columns);
-
-            const handleComplete = (column) => {
-              dispatch({ type: "CARDS", id: column.id });
-            };
-        }
+  // const addCard = () => {
+  //     console.log("Helo")
+  //     const card = {
+  //         id: self.crypto.randomUUID() ,
+  //         title: "New card",
+  //         description: "None"
+  //     }
+  //     dispatch({type:"addCard", card})
+  // }
 
   return (
     <>
       <div>
         <Header></Header>
         <Board>
-            {columns.length > 0 && columns.map((column) => {
-                
-                // function(title, description){
-                // const oldCards = column.cards
-                // const newCard = {title, description}
-                // oldCards.push(newCard)
-            return <Column title={column.title}>
-                {column.cards?.map((card) =>
-                    <Card title={card.title}/>
-                )}
-            </Column>
+          {columns.length > 0 &&
+            columns.map((column, index) => {
+              // function(title, description){
+              // const oldCards = column.cards
+              // const newCard = {title, description}
+              // oldCards.push(newCard)
+              return (
+                <Column
+                  title={column.title}
+                  dispatch={dispatch}
+                  columnNumber={index + 1}
+                >
+                  {column.cards?.map((card) => {
+                    return <Card title={card.title} />;
+                  })}
+                </Column>
+              );
             })}
-            <AddColumn/>
+          <AddColumn />
         </Board>
       </div>
     </>
